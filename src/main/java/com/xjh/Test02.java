7be99d6a96dacf5d6926de094336641a4b941836
xiao <1006963663@qq.com>
2021-07-21 01:11:18 +0800

    first commit

---
+++ src/main/java/com/xjh/Test02.java
@@ -0,0 +1,8 @@
+package com.xjh;
+
+/**
+ * @Author: xJh
+ * @Date: 2021/7/21
+ */
+public class Test02 {
+}
---
+++ src/test/java/com/xjh/XjhApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xjh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XjhApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
